c28bf433e6711407005d42f2533801effef277c3
anubhav-gig <dev.anubhav@gigwalla.com>
2021-10-31 20:41:05 +0530

    Add express routes, refactor to return promises

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const v80Screener = require("./v80Screener");
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/v80", async (req, res) => {
+    const data = await v80Screener(3);
+    const results = data.flat();
+    res.json(results);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
--- v80Screener.js
+++ v80Screener.js
@@ -3,118 +3,125 @@ const { default: parse } = require("node-html-parser");
 const { default: dotenv } = require("dotenv").config();
 
 const stocks = [
-  "CIPLA",
-  "SANOFI",
-  "LALPATHLAB",
-  "RELAXO",
-  "BATAINDIA",
-  "HINDUNILVR",
-  "MARICO",
-  "BRITANNIA",
-  "DABUR",
-  "COLPAL",
-  "NESTLEIND",
-  "ITC",
-  "BAJAJCON",
-  "HEROMOTOCO",
-  "BAJAJ_AUTO",
-  "EICHERMOT",
-  "WHIRLPOOL",
-  "SYMPHONY",
-  "PIDILITIND",
-  "HAVELLS",
-  "PAGEIND",
-  "TITAN",
-  "BAJFINANCE",
-  "BAJAJFINSV",
-  "PGHH",
-  "GILLETTE",
-  "HDFCLIFE",
-  "TCS",
-  "INFY",
-  "WIPRO",
-  "HCLTECH",
-  "AKZOINDIA",
-  "ASIANPAINT",
-  "BERGEPAINT",
-  "ESCORTS",
-  "APLLTD",
-  "CEATLTD",
-  "JKLAKSHMI",
-  "JSWSTEEL",
-  "TATASTEEL",
-  "AMARAJABAT",
-  "ASTRAL",
-  "KOTAKBANK",
-  "HDFCBANK",
-  "ICICIBANK",
-  "NIFTYBEES",
-  "BANKBEES",
-  "HDFCAMC",
-  "ASIANTILES",
-  "NMDC",
-  "EXIDEIND",
-  "###V40 NEXT",
-  "3MINDIA",
-  "AVANTIFEED",
-  "BAYERCROP",
-  "BOSCHLTD",
-  "CAPLIPOINT",
-  "CERA",
-  "DIXON",
-  "EQUITAS",
-  "EQUITASBNK",
-  "ERIS",
-  "FINCABLES",
-  "FINEORG",
-  "GODREJCP",
-  "HONAUT",
-  "ISEC",
-  "JCHAC",
-  "KANSAINER",
-  "MCX",
-  "OFSS",
-  "POLYCAB",
-  "RADICO",
-  "RAJESHEXPO",
-  "SFL",
-  "SIS",
-  "SUNTV",
-  "TASTYBITE",
-  "TEAMLEASE",
-  "TTKPRESTIG",
-  "UJJIVANSFB",
-  "MCDOWELL_N",
-  "VGUARD",
-  "VINATIORGA",
-  "VIPIND"
+    "CIPLA",
+    "SANOFI",
+    "LALPATHLAB",
+    "RELAXO",
+    "BATAINDIA",
+    "HINDUNILVR",
+    "MARICO",
+    "BRITANNIA",
+    "DABUR",
+    "COLPAL",
+    "NESTLEIND",
+    "ITC",
+    "BAJAJCON",
+    "HEROMOTOCO",
+    "BAJAJ_AUTO",
+    "EICHERMOT",
+    "WHIRLPOOL",
+    "SYMPHONY",
+    "PIDILITIND",
+    "HAVELLS",
+    "PAGEIND",
+    "TITAN",
+    "BAJFINANCE",
+    "BAJAJFINSV",
+    "PGHH",
+    "GILLETTE",
+    "HDFCLIFE",
+    "TCS",
+    "INFY",
+    "WIPRO",
+    "HCLTECH",
+    "AKZOINDIA",
+    "ASIANPAINT",
+    "BERGEPAINT",
+    "ESCORTS",
+    "APLLTD",
+    "CEATLTD",
+    "JKLAKSHMI",
+    "JSWSTEEL",
+    "TATASTEEL",
+    "AMARAJABAT",
+    "ASTRAL",
+    "KOTAKBANK",
+    "HDFCBANK",
+    "ICICIBANK",
+    "NIFTYBEES",
+    "BANKBEES",
+    "HDFCAMC",
+    "ASIANTILES",
+    "NMDC",
+    "EXIDEIND",
+    "###V40 NEXT",
+    "3MINDIA",
+    "AVANTIFEED",
+    "BAYERCROP",
+    "BOSCHLTD",
+    "CAPLIPOINT",
+    "CERA",
+    "DIXON",
+    "EQUITAS",
+    "EQUITASBNK",
+    "ERIS",
+    "FINCABLES",
+    "FINEORG",
+    "GODREJCP",
+    "HONAUT",
+    "ISEC",
+    "JCHAC",
+    "KANSAINER",
+    "MCX",
+    "OFSS",
+    "POLYCAB",
+    "RADICO",
+    "RAJESHEXPO",
+    "SFL",
+    "SIS",
+    "SUNTV",
+    "TASTYBITE",
+    "TEAMLEASE",
+    "TTKPRESTIG",
+    "UJJIVANSFB",
+    "MCDOWELL_N",
+    "VGUARD",
+    "VINATIORGA",
+    "VIPIND"
 ];
 
 const url =
-  "https://www.screener.in/screen/raw/?sort=Market+Capitalization&order=&source=334875&query=Current+price+%3C+DMA+200&limit=50&page=";
+    "https://www.screener.in/screen/raw/?sort=Market+Capitalization&order=&source=334875&query=Current+price+%3C+DMA+200&limit=50&page=";
 
-const filteredStocks = [];
+function getQuotes(count) {
+    const promises = [];
 
-for (let i = 1; i <= 20; i++) {
-  axios
-    .get(url + i, {
-      headers: {
-        Cookie: process.env.COOKIE
-      }
-    })
-    .then((resp) => {
-      const root = parse(resp.data);
-      const links = root.querySelectorAll("[data-row-company-name] td a");
-      stocks.map((stock) => {
-        links.map((elem) => {
-          if (elem.rawAttrs.includes(stock)) {
-            filteredStocks.push(elem);
-          }
-        });
-      });
-      if (i === 20) {
-        console.log(filteredStocks.map((elem) => elem.childNodes[0]._rawText));
-      }
-    })
-    .catch((e) => e.message);
+    for (i = 1; i < count; ++i) {
+        promises.push(getData(i));
+    }
+
+    return Promise.all(promises);
 }
+
+const getData = async (i) => {
+    let resp = await axios.get(url + i, {
+        headers: {
+            Cookie: process.env.COOKIE
+        }
+    });
+
+    const root = parse(resp.data);
+    const links = root.querySelectorAll("[data-row-company-name] td a");
+    const filt = [];
+    stocks.map((stock) =>
+        links.map((elem) => {
+            if (elem.rawAttrs.includes(stock)) {
+                filt.push(elem.childNodes[0]._rawText.trim());
+            }
+        })
+    );
+
+    return filt;
+};
+
+module.exports = getQuotes;
